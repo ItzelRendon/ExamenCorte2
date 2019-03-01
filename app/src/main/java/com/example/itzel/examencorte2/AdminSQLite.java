ea247fc668714ef32b28978ed471fbf823d9699e
ItzelRendon <2016030038@upsin.edu.mx>
2019-03-01 08:32:05 -0700

    Proyecto Chingón :100:

---
+++ app/src/main/java/com/example/itzel/examencorte2/AdminSQLite.java
@@ -0,0 +1,22 @@
+package com.example.itzel.examencorte2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class AdminSQLite extends SQLiteOpenHelper {
+    public AdminSQLite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table respuestas(codigo integer primary key autoincrement, pregunta1 text, pregunta2 text, pregunta3 text, pregunta4 text, pregunta5 text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
